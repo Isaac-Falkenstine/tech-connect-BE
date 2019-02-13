e572cf5060e5bd8ebe71e7afda4ea45c7dfe86d8
Isaac Falkenstine <isaac@falkenstine.com>
2019-02-13 14:22:10 -0700

    Adds sad path test for POST to users

--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'making a user api and response' do
 
-  it '/api/v1/users' do
+  it 'POST /api/v1/users' do
     post '/api/v1/users?email=email_address@example.com&password=password&password_confirmation=password'
 
     expect(response).to be_successful
@@ -11,5 +11,15 @@ describe 'making a user api and response' do
 
     expect(parsed[:data][:attributes][:api_key]).to eq(User.last[:api_key])
     expect(User.last.email).to eq('email_address@example.com')
+  end
+
+  it 'POST /api/v1/users duplicate' do
+    post '/api/v1/users?email=email_address@example.com&password=password&password_confirmation=password'
+    post '/api/v1/users?email=email_address@example.com&password=password&password_confirmation=password'
+    expect(response).not_to be_successful
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed[:error]).to eq("It looks like a user is already using that email! Please try again.")
   end
 end
