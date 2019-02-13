252245bc46dfe60e7eee2724665dc40a07d676e6
Isaac Falkenstine <isaac@falkenstine.com>
2019-02-13 13:53:42 -0700

    Write test for post to users

---
+++ spec/requests/api/v1/user_request_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'making a user api and response' do
+
+  it '/api/v1/users' do
+    post '/api/v1/users?email=email_address@example.com&password=password&password_confirmation=password'
+
+    expect(response).to be_successful
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed[:data][:attributes][:api_key]).to eq(User.last[:api_key])
+    expect(User.last.email).to eq('email_address@example.com')
+  end
+end
