3c40cce89547197400843d589dc8744955b11a98
Bailey Diveley <bailey.diveley@gmail.com>
2019-02-13 17:17:23 -0700

    Test sad path if user login credentials are invalid

--- spec/requests/api/v1/login_request_spec.rb
+++ spec/requests/api/v1/login_request_spec.rb
@@ -12,7 +12,9 @@ describe 'user login request and response' do
     parsed = JSON.parse(response.body, symbolize_names: true)
 
     expect(parsed[:data][:attributes]).to have_key(:api_key)
+    expect(parsed[:data][:attributes][:api_key]).to eq(user.api_key)
     expect(parsed[:data][:attributes]).to have_key(:email)
+    expect(parsed[:data][:attributes][:email]).to eq(user.email)
     expect(parsed[:data][:attributes]).to have_key(:name)
     expect(parsed[:data][:attributes]).to have_key(:github)
     expect(parsed[:data][:attributes]).to have_key(:linkedin)
@@ -21,6 +23,12 @@ describe 'user login request and response' do
     expect(parsed[:data][:attributes]).to have_key(:location)
     expect(parsed[:data][:attributes]).to have_key(:position)
     expect(parsed[:data][:attributes]).to have_key(:employer)
+  end
+  it 'POST /api/v1/login is unsuccessful if user input is invalid' do
+    params = {email: "fake@email.com", password: "password"}
+    post '/api/v1/login',  params: params
 
+    expect(response).not_to be_successful
+    expect(response.body).to eq("Unauthorized")
   end
 end
