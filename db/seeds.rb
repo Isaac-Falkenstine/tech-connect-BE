ac83adf61abca05ff13f81a93ff8f0840c5e3b46
Isaac Falkenstine <isaac@falkenstine.com>
2019-02-14 15:58:39 -0700

    Adds a lot of fake users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,16 @@
+require 'faker'
+
 Location.create(city: "N/A")
 Employer.create(name: "Unemployed", site_url: "N/A")
 Position.create(job_title: "Unemployed")
 
-registered = User.create(id: 1, name: "Registered Joe", email: "registered@email.com", password: "password123")
-second_user = User.create(id: 3, name: "Second Joe", email: "second@email.com", password: "password123")
+registered = User.create(name: "Registered Joe", email: "registered@email.com", password: "password123")
+second_user = User.create(name: "Second Joe", email: "second@email.com", password: "password123")
 
-location = Location.create(city: "Denver,CO")
-employer = Employer.create(name: "Turing", site_url: "turing.io")
-position = Position.create(job_title: "Employee")
-updated = User.create(id: 2,
+denver = Location.create(city: "Denver,CO")
+turing = Employer.create(name: "Turing", site_url: "turing.io")
+employee = Position.create(job_title: "Employee")
+updated = User.create(
             name: "Updated Joe",
             email: "updated@email.com",
             password: "password123",
@@ -17,9 +19,85 @@ updated = User.create(id: 2,
             linkedin: "linkedin.com/users/joe",
             bio:"Hey everyone, my name is Joe! I'm really trying to get into the tech world! HELP!",
             api_key: "1234567891011121",
-            location_id: location.id,
-            employer_id: employer.id,
-            position_id: position.id)
+            location_id: denver.id,
+            employer_id: turing.id,
+            position_id: employee.id)
 
 message_1 = Message.create(user_id: registered.id, connection_id: updated.id, status: "Confirmed", meeting_date: DateTime.new(2001,2,3,4,5,6), meeting_location: "Starbucks on Colorado")
 message_2 = Message.create(user_id: registered.id, connection_id: second_user.id, status: "Confirmed", meeting_date: DateTime.new(2002,2,6,9,5,1), meeting_location: "Dazbog on Blake")
+
+# Everyone in Denver
+  4.times do
+    company = Faker::Company.name
+    employer = Employer.create(name: Faker::Company.name, site_url: "#{company}.com")
+    position = Position.create(job_title: Faker::Company.profession)
+
+    User.create(name: Faker::FunnyName.two_word_name,
+                email: Faker::Internet.email,
+                password: Faker::Internet.password,
+                phone_number: rand(1000000000..9999999999),
+                github: Faker::Internet.url('github.com'),
+                linkedin: Faker::Internet.url('linkedin.com'),
+                bio: Faker::Movies::StarWars.quote,
+                api_key: SecureRandom.urlsafe_base64(12),
+                location_id: denver.id,
+                employer_id: employer.id,
+                position_id: position.id)
+  end
+  #Everyone at google
+    4.times do
+      employer = Employer.create(name: "Google", site_url: "google.com")
+      position = Position.create(job_title: Faker::Company.profession)
+
+      User.create(name: Faker::FunnyName.two_word_name,
+                  email: Faker::Internet.email,
+                  password: Faker::Internet.password,
+                  phone_number: rand(1000000000..9999999999),
+                  github: Faker::Internet.url('github.com'),
+                  linkedin: Faker::Internet.url('linkedin.com'),
+                  bio: Faker::Movies::StarWars.quote,
+                  api_key: SecureRandom.urlsafe_base64(12),
+                  location_id: denver.id,
+                  employer_id: employer.id,
+                  position_id: position.id)
+    end
+    # Everyone as a software developer
+      4.times do
+        employer = Employer.create(name: "Google", site_url: "google.com")
+        position = Position.create(job_title: "Backend Software Developer")
+
+        User.create(name: Faker::FunnyName.two_word_name,
+                    email: Faker::Internet.email,
+                    password: Faker::Internet.password,
+                    phone_number: rand(1000000000..9999999999),
+                    github: Faker::Internet.url('github.com'),
+                    linkedin: Faker::Internet.url('linkedin.com'),
+                    bio: Faker::Movies::StarWars.quote,
+                    api_key: SecureRandom.urlsafe_base64(12),
+                    location_id: denver.id,
+                    employer_id: employer.id,
+                    position_id: position.id)
+      end
+    # end Backend Software Developer
+  # end google
+# end denver
+
+
+12.times do
+  company = Faker::Company.name
+  location = Location.create(city: Faker::Movies::StarWars.planet)
+  employer = Employer.create(name: Faker::Company.name, site_url: "#{company}.com")
+  position = Position.create(job_title: Faker::Company.profession)
+
+  User.create(name: Faker::FunnyName.two_word_name,
+              email: Faker::Internet.email,
+              password: Faker::Internet.password,
+              phone_number: rand(1000000000..9999999999),
+              github: Faker::Internet.url('github.com'),
+              linkedin: Faker::Internet.url('linkedin.com'),
+              bio: Faker::GreekPhilosophers.quote,
+              api_key: SecureRandom.urlsafe_base64(12),
+              location_id: location.id,
+              employer_id: employer.id,
+              position_id: position.id)
+end
