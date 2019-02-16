a99fd1ec74fc6f4c179ec462d43aec7c832e0006
Isaac Falkenstine <isaac@falkenstine.com>
2019-02-16 10:07:31 -0700

    Write sad path test for deleting a user if no api_key is passed in

--- spec/requests/api/v1/user_delete_spec.rb
+++ spec/requests/api/v1/user_delete_spec.rb
@@ -16,4 +16,16 @@ describe 'deleting a user request and response' do
     expect(User.all.length).to eq(0)
     expect(parsed[:message]).to eq("Isaac Falkenstine has been deleted")
   end
+
+  it 'DELETE /api/v1/users/:id without api_key' do
+    user_1 = create(:user, name: "Isaac Falkenstine")
+
+    delete "/api/v1/users/#{user_1.id}"
+
+    expect(response).not_to be_successful
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed[:error]).to eq("Unauthorized")
+  end
 end
