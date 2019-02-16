e5f8051a16687d87270eb9312597bc6e2ea417ee
Isaac Falkenstine <isaac@falkenstine.com>
2019-02-16 09:58:56 -0700

    Write test for deleting user endpoint

---
+++ spec/requests/api/v1/user_delete_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'deleting a user request and response' do
+
+  it 'DELETE /api/v1/users/:id with api_key' do
+    user_1 = create(:user, name: "Isaac Falkenstine")
+
+    params = {api_key: user_1.api_key}
+
+    delete "/api/v1/users/#{user_1.id}", params: params
+
+    expect(response).to be_successful
+    expect(response).to have_http_status(202)
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(User.all.length).to eq(0)
+    expect(parsed[:message]).to eq("Isaac Falkenstine has been deleted")
+  end
+end
