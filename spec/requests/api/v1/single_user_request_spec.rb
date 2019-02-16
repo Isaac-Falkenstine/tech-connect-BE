f992d7271024b87610d05874da8618c7f194abb3
Isaac Falkenstine <isaac@falkenstine.com>
2019-02-15 20:36:30 -0700

    Write test for user request with no api_key

--- spec/requests/api/v1/single_user_request_spec.rb
+++ spec/requests/api/v1/single_user_request_spec.rb
@@ -2,20 +2,37 @@ require 'rails_helper'
 
 describe 'making a position api and response' do
 
-  it 'GET /api/v1/users' do
+  it 'GET /api/v1/users/id with api key' do
     user_1 = create(:user, name: "Isaac Falkenstine")
     create(:user, name: "Bailey Diveley")
 
     params = {api_key: user_1.api_key}
 
-    get '/api/v1/users', params: params
+    get "/api/v1/users/#{user_1.id}", params: params
 
     expect(response).to be_successful
     parsed = JSON.parse(response.body, symbolize_names: true)
 
-    expect(parsed[:data]).to be_a(Array)
-    expect(parsed[:data].length).to eq(1)
-    expect(parsed[:data][0][:attributes]).to have_key(:name)
-    expect(parsed[:data][0][:attributes][:name]).to eq("Isaac Falkenstine")
+    expect(parsed[:data][:attributes]).to have_key(:name)
+    expect(parsed[:data][:attributes]).to have_key(:phone_number)
+    expect(parsed[:data][:attributes]).to have_key(:email)
+
+    expect(parsed[:data][:attributes][:name]).to eq("Isaac Falkenstine")
+  end
+
+  it 'GET /api/v1/users/id without api_key' do
+    user_1 = create(:user, name: "Isaac Falkenstine")
+    create(:user, name: "Bailey Diveley")
+
+    get "/api/v1/users/#{user_1.id}"
+
+    expect(response).to be_successful
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed[:data][:attributes]).to have_key(:name)
+    expect(parsed[:data][:attributes]).not_to have_key(:phone_number)
+    expect(parsed[:data][:attributes]).not_to have_key(:email)
+
+    expect(parsed[:data][:attributes][:name]).to eq("Isaac Falkenstine")
   end
 end
