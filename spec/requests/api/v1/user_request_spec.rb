f661071182fc62824ee42de10d235e93efabc8c3
Bailey Diveley <bailey.diveley@gmail.com>
2019-02-16 10:43:21 -0700

    Add filter name sad path test

--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -34,4 +34,40 @@ describe 'making a position api and response' do
 
     expect(parsed[:error]).to eq("Unauthorized")
   end
+  it 'GET /api/v1/users/find?name=Bailey' do
+    user_1 = create(:user, name: "Isaac Falkenstine")
+    create(:user, name: "Bailey Diveley")
+    create(:user, name: "Kaylee Bunce")
+    create(:user, name: "Kylie Stanley")
+
+    params = {api_key: user_1.api_key}
+
+    get '/api/v1/users/find?name=Bailey', params: params
+
+    expect(response).to be_successful
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed[:data]).to be_a(Array)
+    expect(parsed[:data].length).to eq(1)
+    expect(parsed[:data][0][:attributes]).to have_key(:name)
+    expect(parsed[:data][0][:attributes][:name]).to eq("Bailey Diveley")
+  end
+  it 'GET /api/v1/users/find?name=bailey' do
+    user_1 = create(:user, name: "Isaac Falkenstine")
+    create(:user, name: "Bailey Diveley")
+    create(:user, name: "Kaylee Bunce")
+    create(:user, name: "Kylie Stanley")
+
+    params = {api_key: user_1.api_key}
+
+    get '/api/v1/users/find?name=bailey', params: params
+
+    expect(response).to be_successful
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed[:data]).to be_a(Array)
+    expect(parsed[:data].length).to eq(1)
+    expect(parsed[:data][0][:attributes]).to have_key(:name)
+    expect(parsed[:data][0][:attributes][:name]).to eq("Bailey Diveley")
+  end
 end
