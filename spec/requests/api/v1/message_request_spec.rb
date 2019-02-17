f20c22e7b6277562d781a9a4a4965d72e4f92887
Bailey Diveley <bailey.diveley@gmail.com>
2019-02-17 08:33:15 -0700

    Set up get all messages test

---
+++ spec/requests/api/v1/message_request_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+describe 'making a message api and response' do
+
+  it 'GET /api/v1/messages' do
+    user = create(:user)
+    create(:message, user_id: user.id)
+    create(:message, user_id: user.id)
+    create(:message, user_id: user.id)
+    params = {api_key: user.api_key}
+
+    get '/api/v1/messages', params: params
+
+    expect(response).to be_successful
+    parsed = JSON.parse(response.body, symbolize_names: true)
+    expect(parsed[:data]).to be_a(Array)
+    expect(parsed[:data].length).to eq(3)
+    expect(parsed[:data][0][:attributes]).to have_key(:status)
+    expect(parsed[:data][0][:attributes]).to have_key(:meeting_date)
+    expect(parsed[:data][0][:attributes]).to have_key(:meeting_location)
+    expect(parsed[:data][0][:attributes]).to have_key(:user_id)
+    #should also return connection_id's name
+    expect(parsed[:data][0][:attributes]).to have_key(:connection_id)
+  end
+end
