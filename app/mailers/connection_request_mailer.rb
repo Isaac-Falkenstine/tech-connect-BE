6970c16608b7576faa1b6cf913d4e5712a36b268
Bailey Diveley <bailey.diveley@gmail.com>
2019-02-18 13:54:10 -0700

    Edited action in email form to use url_helper instead of development url

--- app/mailers/connection_request_mailer.rb
+++ app/mailers/connection_request_mailer.rb
@@ -1,13 +1,13 @@
 class ConnectionRequestMailer < ApplicationMailer
 
   def connect(data)
-    @email = User.find(data[:connection_id]).email
+    @connection = User.find(data[:connection_id])
     @user = User.find_by(api_key: data[:api_key])
     @data = data
     @data[:datetime_1] = Time.parse(data[:datetime_1]).strftime("%m/%d/%Y %I:%M %p")
     @data[:datetime_2] = Time.parse(data[:datetime_2]).strftime("%m/%d/%Y %I:%M %p")
     @data[:datetime_3] = Time.parse(data[:datetime_3]).strftime("%m/%d/%Y %I:%M %p")
 
-    mail(to: @email, subject: "#{@user.name} is requesting a connection")
+    mail(to: @connection.email, subject: "#{@user.name} is requesting a connection")
   end
 end
