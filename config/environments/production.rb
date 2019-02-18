24cab3a3a14300bcf6d5fdeea6ff40d3bf69be13
Bailey Diveley <bailey.diveley@gmail.com>
2019-02-18 14:03:37 -0700

    Edit production url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'https://tech-connect-be.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'https://tech-connect-be.herokuapp.com' }
   config.action_mailer.smtp_settings = {
     address:              'smtp.sendgrid.net',
     port:                 '587',
