d41e98d527744ed489b4fa3371b771b0da113f79
Isaac Falkenstine <isaac@falkenstine.com>
2019-02-17 20:51:50 -0700

    Add rain check to emails

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -10,16 +10,25 @@ class Api::V1::MessagesController < ApplicationController
   end
 
   def create
-    if params[:status] == "confirmed"
-      Message.create(message_params)
-    end
+    Message.create(message_params)
   end
 
 private
   def message_params
-    update_info = params.permit(:meeting_location, :meeting_date, :status, :user_id)
-    id = User.where(email: params[:email])[0].id
-    update_info[:connection_id] =id
-    return update_info
+    if params.has_key?("make-meeting")
+      update_info = params.permit(:meeting_location, :meeting_date, :user_id)
+      id = User.where(email: params[:email])[0].id
+      update_info[:connection_id] =id
+      return update_info
+    else
+      update_info = params.permit(:user_id)
+      id = User.where(email: params[:email])[0].id
+      update_info[:connection_id] =id
+      update_info[:status] = "declined"
+      update_info[:meeting_location] = "N/A"
+      update_info[:meeting_date] = DateTime.new(2000,1,1,1,1,1)
+
+      return update_info
+    end
   end
 end
