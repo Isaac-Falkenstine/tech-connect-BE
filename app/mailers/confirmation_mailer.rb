6ae79451c56135f6230046278c28ba9822866a01
Bailey Diveley <bailey.diveley@gmail.com>
2019-02-18 09:15:18 -0700

    Update formatting to confirmation emails

--- app/mailers/confirmation_mailer.rb
+++ app/mailers/confirmation_mailer.rb
@@ -1,18 +1,17 @@
 class ConfirmationMailer < ApplicationMailer
 
   def confirm(data)
-    @user_email = User.find(data[:user_id]).email
-    @connection_email = User.find(data[:connection_id]).email
+    @user = User.find(data[:user_id])
+    @connection = User.find(data[:connection_id])
     @data = data
-    mail(to: @user_email, subject: "You have a confirmed meeting with #{@connection_email}")
-    mail(to: @connection_email, subject: "You have a confirmed meeting with #{@user_email}")
+    mail(to: [@user.email, @connection.email], subject: "You have a confirmed meeting through TechConnect")
   end
 
   def decline(data)
     @user_email = User.find(data[:user_id]).email
     @connection_name = User.find(data[:connection_id]).name
     @data = data
-    mail(to: @user_email, subject: "#{@connection_name} must Raincheck your requested meeting")
+    mail(to: @user_email, subject: "#{@connection_name} requests a raincheck")
   end
 
 end
