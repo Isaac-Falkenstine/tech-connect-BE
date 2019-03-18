f987116e8f9d8b7e3d26e93f924fc5b7e840ce99
Bailey Diveley <bailey.diveley@gmail.com>
2019-03-18 11:23:27 -0600

    Add token to message factorybot

--- spec/factories/message.rb
+++ spec/factories/message.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     status   { |n| "Status #{n}" }
     meeting_date { |n| DateTime.new(2001,2,3,4,5,6) }
     meeting_location { |n| "Meeting Location #{n}" }
-
+    token { "abc123" }
     user
     connection {|n| user }
   end
